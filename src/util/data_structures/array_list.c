c2b0c49b3295cc02d1753daf616b849cab98d571
Alexandru Chitoiu-Marin <a.chitoiu-marin@student.tudelft.nl>
2026-05-05 23:26:59 +0200

    Create add_at_index_array_list_function

--- src/util/data_structures/array_list.c
+++ src/util/data_structures/array_list.c
@@ -14,26 +14,75 @@ typedef struct array_list {
 Constructor
 Allocate memory for the stack buffer and set the initial size
 @param stack Pointer to a stack struct
+@return Pointer to a new array list struct
 */
 array_list_s* initialize_array_list() {
     array_list_s* list = malloc(sizeof(array_list_s));
     list->size = 0;
-    list->capacity = 100 * sizeof(void**);
-    list->array = malloc(list->capacity);
+    list->capacity = 10;
+    list->array = malloc(list->capacity * sizeof(void*));
     return list;
 }
 
 /*
-Check if the array list is at full capacity
-@param list Pointer to a array list struct
+Private function used to check if the array list is at full capacity
+@param list Pointer to an array list struct
+@return true if the array list is full and false otherwise
 */
-int is_full(array_list_s* list) {
-    int occupied = list->capacity / sizeof(void**);
-    if(list->size >= occupied);
+static int is_full(array_list_s* list) {
+    if(list->size >= list->capacity)
         return 1;
     return 0;
 }
 
+/*
+Private function used to double the capacity of the array
+@param list Pointer to an array list struct
+*/
+static void increase_capacity(array_list_s* list) {
+    size_t new_capacity = list->capacity * 2;
+    void** new_array = realloc(list->array, new_capacity * sizeof(void*));
+    if (new_array == NULL) {
+        printf("Could not increase capacity of array list");
+        return; 
+    }
+    list->array = new_array;
+    list->capacity = new_capacity;
+}
+
+/*
+Check if the array list is empty
+@param list Pointer to a array list struct
+@return true if the array list is empty and false otherwise
+*/
+int is_empty_array_list(array_list_s* list) {
+    return list->size == 0;
+}
+
+/*
+Adds an element after a specific index
+@param list Pointer to array list struct
+@param index Index in the array list to insert the element
+*/
+void add_at_index_array_list(array_list_s* list, int index, void* element) {
+    if(index < 0) {
+        printf("Array list index out of bounds!");
+        return;
+    }
+    
+    if(index > list->size) 
+        index = list->size;
+    
+    if(is_full(list))
+        increase_capacity(list);
+    
+    void** arr = list->array;
+    for(int i = list->size; i > index; i--) 
+        arr[i] = arr[i-1];
+
+    arr[index] = element;
+    list->size++;
+}
 
 /*
 Destructor
