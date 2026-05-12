20e315963f09c0e770ef783485b592b993f49e07
Alexandru Chitoiu-Marin <a.chitoiu-marin@student.tudelft.nl>
2026-05-12 17:21:06 +0200

    Fix generic dll and test for generic dll

--- inc/dll.h
+++ inc/dll.h
@@ -1,5 +1,5 @@
-#ifndef LEXER_H
-#define LEXER_H
+#ifndef DLL_H
+#define DLL_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,21 +11,41 @@
 #define DLL_CONCAT(TAG, METHOD) TAG##_##METHOD
 #define DLL_CONCAT_EXP(TAG, METHOD) DLL_CONCAT(TAG, METHOD)
 #define DLL_FN(METHOD) DLL_CONCAT_EXP(DLL_TAG, METHOD)
+#define NODE_TAG DLL_CONCAT_EXP(DLL_TAG, node)
+
             
 /* Node definition for dll implementation*/ 
-typedef struct dll_node {                       
-    struct dll_node* next;                      
-    struct dll_node* prev;                      
+typedef struct NODE_TAG {                       
+    struct NODE_TAG* next;                      
+    struct NODE_TAG* prev;                      
     DLL_ITEM_TYPE payload;                           
-} dll_node;                                   
+} NODE_TAG;                                   
                                             
 /*DLL definition with sentinel head and tail*/  
 typedef struct DLL_TAG {                            
-     dll_node* head;                               
-     dll_node* tail;                               
+     struct NODE_TAG* head;                               
+     struct NODE_TAG* tail;                               
     int size;                                   
-} DLL_TAG;                                         
-                                                
+} DLL_TAG;
+
+/*                                                       
+Set the next node                                        
+@param node Node that has its next parameter set         
+@param next Node that is pointed to by the attribute     
+*/                                                       
+void DLL_FN(set_next)( NODE_TAG* node,  NODE_TAG* next) {     
+    node->next = next;                                                       
+}                                                                            
+                                                                             
+/*                                                           
+Set the prev node                                            
+@param node Node that has its prev parameter set             
+@param prev Node that is pointed to by the attribute         
+*/                                                           
+void DLL_FN(set_prev)( NODE_TAG* node,  NODE_TAG* prev) {     
+    node->prev = prev;                                                       
+}   
+
 /*                                              
 Create a new node                               
 @param payload Pointer to the node payload      
@@ -33,15 +53,15 @@ Create a new node
 @param next The next node                       
 @return Pointer to the node                     
 */                                              
-static dll_node* initialize_node(DLL_ITEM_TYPE payload,  dll_node* prev,  dll_node* next) {    
-     dll_node* node = malloc(sizeof( dll_node));              
+NODE_TAG* DLL_FN(initialize_node)(DLL_ITEM_TYPE payload,  NODE_TAG* prev,  NODE_TAG* next) {    
+     NODE_TAG* node = malloc(sizeof( NODE_TAG));              
     if(!node) {     
         fprintf(stderr, "FATAL: Could not allocate memory for DLL node!");                                             
-        return NULL;
+        exit(EXIT_FAILURE);
     }                                            
     node->payload = payload;                                       
-    set_next(node, next);                                    
-    set_prev(node, prev);                                    
+    DLL_FN(set_next)(node, next);                                    
+    DLL_FN(set_prev)(node, prev);                                    
                                                                  
     return node;                                                 
 }                                                                
@@ -51,7 +71,7 @@ Get the node data payload
 @param node Node struct                  
 @return The data payload from the node   
 */                                       
-static DLL_ITEM_TYPE get_payload(dll_node* node) {    
+DLL_ITEM_TYPE DLL_FN(get_payload)(NODE_TAG* node) {    
     return node->payload;                                    
 }                                                            
                                                              
@@ -60,7 +80,7 @@ Get the next node
 @param node The reference node                               
 @return The next node                                        
 */                                                           
-static dll_node* get_next( dll_node* node) {    
+NODE_TAG* DLL_FN(get_next)( NODE_TAG* node) {    
     return node->next;                                           
 }                                                                
                                                                  
@@ -69,33 +89,15 @@ Get the prev node
 @param node The reference node       
 @return The prev node                
 */                                   
-static dll_node* get_prev( dll_node* node) {        
+NODE_TAG* DLL_FN(get_prev)( NODE_TAG* node) {        
     return node->prev;                                               
 }                                                                    
-                                                                     
-/*                                                       
-Set the next node                                        
-@param node Node that has its next parameter set         
-@param next Node that is pointed to by the attribute     
-*/                                                       
-static void set_next( dll_node* node,  dll_node* next) {     
-    node->next = next;                                                       
-}                                                                            
-                                                                             
-/*                                                           
-Set the prev node                                            
-@param node Node that has its prev parameter set             
-@param prev Node that is pointed to by the attribute         
-*/                                                           
-static void set_prev( dll_node* node,  dll_node* prev) {     
-    node->prev = prev;                                                       
-}                                                                            
-                                                                             
+                                                                                                                                        
 /*                                               
 Destroy a node                                   
 @param Pointer to a pointer of a node struct     
 */                                               
-static void destroy_node( dll_node** node) {      
+void DLL_FN(destroy_node)( NODE_TAG** node) {      
     if((*node) == NULL || node == NULL)                              
         return;                                                      
                                                                      
@@ -111,19 +113,18 @@ DLL_TAG* DLL_FN(initialize)(void) {
     DLL_TAG* dll = malloc(sizeof(DLL_TAG));            
     if(dll == NULL) {
         fprintf(stderr, "FATAL: Could not allocate memory for DLL!");                             
-        return NULL; 
+        exit(EXIT_FAILURE); 
     }                            
                                                  
     DLL_ITEM_TYPE dummy_payload;                          
     memset(&dummy_payload, 0, sizeof(DLL_ITEM_TYPE));                             
-    dll->head = initialize_node(dummy_payload, NULL, NULL);       
-    dll->tail = initialize_node(dummy_payload, NULL, NULL);       
+    dll->head = DLL_FN(initialize_node)(dummy_payload, NULL, NULL);       
+    dll->tail = DLL_FN(initialize_node)(dummy_payload, dll->head, NULL);   
+    DLL_FN(set_next)(dll->head, dll->tail);    
                                                                  
     if (!dll->head || !dll->tail)                                
         return NULL;                                             
-                                                                 
-    set_prev(dll->tail, dll->head);       
-    set_next(dll->head, dll->tail);       
+                                                                       
     dll->size = 0;                               
     return dll;                                  
 }                                                
@@ -144,11 +145,11 @@ Insert a node between two nodes
 @param prev The node before the inserted node    
 @param next The node after the inserted node     
 */                                               
-void  DLL_FN(insert_between)(DLL_TAG* dll, DLL_ITEM_TYPE payload,  dll_node* prev,  dll_node* next) {      
-     dll_node* node = initialize_node(payload, prev, next);                                             
+void  DLL_FN(insert_between)(DLL_TAG* dll, DLL_ITEM_TYPE payload,  NODE_TAG* prev,  NODE_TAG* next) {      
+     NODE_TAG* node = DLL_FN(initialize_node)(payload, prev, next);                                             
                                                                                                              
-    set_prev(next, node);                                                                             
-    set_next(prev, node);                                                                             
+    DLL_FN(set_prev)(next, node);                                                                             
+    DLL_FN(set_next)(prev, node);                                                                             
     dll->size++;                                                                                             
 }                                                                                                            
                                                                                                              
@@ -158,7 +159,7 @@ Insert a node right after the sentinel head
 @payload Pointer to the node payload             
 */                                               
 void DLL_FN(insert_first)(DLL_TAG* dll, DLL_ITEM_TYPE payload) {              
-    DLL_FN(insert_between)(dll, payload, dll->head, get_next(dll->head));  
+    DLL_FN(insert_between)(dll, payload, dll->head, DLL_FN(get_next)(dll->head));  
 }                                                                                
                                                                                  
 /*                                                                               
@@ -167,7 +168,7 @@ Insert a node right before the sentinel tail
 @payload Pointer to the node payload                                             
 */                                                                               
 void DLL_FN(insert_last)(DLL_TAG* dll, DLL_ITEM_TYPE payload) {                       
-    DLL_FN(insert_between)(dll, payload, get_prev(dll->tail), dll->tail);          
+    DLL_FN(insert_between)(dll, payload, DLL_FN(get_prev)(dll->tail), dll->tail);          
 }                                                                                        
                                                                                          
 /*                                   
@@ -175,17 +176,17 @@ Remove a node from the list
 @param dll Pointer to DLL struct     
 @param node Node that is removed     
 */                                   
-DLL_ITEM_TYPE DLL_FN(remove_node)(DLL_TAG* dll,  dll_node* node) {    
+DLL_ITEM_TYPE DLL_FN(remove_node)(DLL_TAG* dll,  NODE_TAG* node) {    
     if(dll->size == 0) {                                             
         fprintf(stderr, "ERROR: DLL is empty, node can't be removed!");              
-        return NULL;                                                 
+        return (DLL_ITEM_TYPE) 0;                                                 
     }                                                                
-     dll_node* prev = get_prev(node);                        
-     dll_node* next = get_next(node);                        
-    DLL_ITEM_TYPE payload = get_payload(node);                            
-    set_next(prev, next);                                     
-    set_prev(next, prev);                                     
-    destroy_node(&node);                                      
+     NODE_TAG* prev = DLL_FN(get_prev)(node);                        
+     NODE_TAG* next = DLL_FN(get_next)(node);                        
+    DLL_ITEM_TYPE payload = DLL_FN(get_payload)(node);                            
+    DLL_FN(set_next)(prev, next);                                     
+    DLL_FN(set_prev)(next, prev);                                     
+    DLL_FN(destroy_node)(&node);                                      
     dll->size--;                                                     
     return payload;                                                  
 }                                                                    
@@ -195,7 +196,7 @@ Remove the first node after the DLL sentinel head
 @param dll Pointer to DLL struct                     
 */                                                   
 DLL_ITEM_TYPE DLL_FN(remove_first)(DLL_TAG* dll) {              
-    return DLL_FN(remove_node)(dll, get_next(dll->head));     
+    return DLL_FN(remove_node)(dll, DLL_FN(get_next)(dll->head));     
 }                                                                
                                                                  
 /*                                                               
@@ -203,7 +204,7 @@ Remove the first node before the DLL sentinel tail
 @param dll Pointer to DLL struct                                 
 */                                                               
 DLL_ITEM_TYPE DLL_FN(remove_last)(DLL_TAG* dll) {               
-    return DLL_FN(remove_node)(dll, get_prev(dll->tail));     
+    return DLL_FN(remove_node)(dll, DLL_FN(get_prev)(dll->tail));     
 }                                                                
                                                                  
 /*                                                               
@@ -212,9 +213,9 @@ Get the payload of the first node in the DLL
 */                                                               
 DLL_ITEM_TYPE DLL_FN(get_first)(DLL_TAG* dll) {                    
     if(dll->size != 0)                                           
-        return get_payload(get_next(dll->head));   
+        return DLL_FN(get_payload)(DLL_FN(get_next)(dll->head));   
     fprintf(stderr, "ERROR: DLL is empty, can't retrieve payload!");             
-    return NULL;                                                 
+    return (DLL_ITEM_TYPE) 0;
 }                                                                
                                                                  
 /*                                                               
@@ -223,9 +224,9 @@ Get the payload of the first node in the DLL
 */                                                               
 DLL_ITEM_TYPE DLL_FN(get_last)(DLL_TAG* dll) {                  
     if(dll->size != 0)                                           
-        return get_payload(get_prev(dll->tail));      
+        return DLL_FN(get_payload)(DLL_FN(get_prev)(dll->tail));      
     fprintf(stderr, "ERROR: DLL is empty, can't retrieve payload!");             
-    return NULL;                                                 
+    return (DLL_ITEM_TYPE) 0;                                                 
 }                                                                
                                                                  
 /*                                                               
@@ -236,10 +237,10 @@ void DLL_FN(destroy)(DLL_TAG** dll) {
     if (dll == NULL || (*dll) == NULL)                           
         return;                                                  
                                                                  
-     dll_node* current = (*dll)->head;                          
+     NODE_TAG* current = (*dll)->head;                          
     while(current != NULL) {                                     
-         dll_node* next = get_next(current);             
-        destroy_node(&current);                           
+         NODE_TAG* next = DLL_FN(get_next)(current);             
+        DLL_FN(destroy_node)(&current);                           
         current = next;                                          
     }                                                                 
     free((*dll));                                                
@@ -251,5 +252,6 @@ void DLL_FN(destroy)(DLL_TAG** dll) {
 #undef DLL_CONCAT
 #undef DLL_CONCAT_EXP
 #undef DLL_FN
+#undef NODE_TAG
 
 #endif
