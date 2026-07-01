6091831b66012dd8151b79cb814de51120cdf176
Alexandru Chitoiu-Marin <alexandru.chitoiu10@gmail.com>
2026-07-01 17:01:42 +0200

    Define global variables and data structures for lexer

--- src/engine/lexer/lexer.c
+++ src/engine/lexer/lexer.c
@@ -3,6 +3,26 @@
 #include <unistd.h>
 #include <string.h>
 #include "lexer.h"
+#include "token.h"
+
+// Setup for Token Array List
+#define ARRAY_LIST_ITEM_TYPE token_s
+#define ARRAY_LIST_TAG token_list
+#include "array_list.h"
+
+/* Global Variables 
+source - String containing the script
+tokens - Token Array List
+start -
+current -
+line -
+*/
+
+char* source;
+token_list* tokens;
+int start;
+int current;
+int line;
 
 void lex(line_s* line) {
 
