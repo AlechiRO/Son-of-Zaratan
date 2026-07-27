e8054e2572869d44ea0e458ea7a7adad5a41164e
Alexandru Chitoiu-Marin <alexandru.chitoiu10@gmail.com>
2026-07-27 17:32:56 +0300

    Create structure of generic hashmap test

---
+++ tests/generics/test_hashmap.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include <stdlib.h>
+#include <err.h>
+#include <string.h>
+
+#define DLL_ITEM_TYPE int
+#define DLL_TAG int_dll
+#include "dll.h"
+
+// Global dll struct pointer
+int_dll* dll;
+
+/*
+Helper function to free the memory for the dll
+*/
+static void clean_up(void) {
+    
+}
+/*
+Helper function to initialize the dll
+*/
+static void set_up(void) {
+    
+}
+
+/* 
+Helper function to create a suite
+@param name Pointer to the name of the suite
+@return CUnit suite object
+*/
+static CU_pSuite create_suite(const char* name,  void(*set_up)(),  void(*tear)()) {
+    CU_pSuite suite = CU_add_suite_with_setup_and_teardown(name, NULL, NULL, set_up, tear); 
+    if (CU_get_error() != CUE_SUCCESS)
+        errx(EXIT_FAILURE, "%s", CU_get_error_msg());
+    return suite;
+}
+
+
+
+int main(void) {
+
+    // initialize registry
+    if (CU_initialize_registry() != CUE_SUCCESS)
+        errx(EXIT_FAILURE, "can't initialize test registry");
+
+
+    // run the tests
+    CU_basic_run_tests();
+
+    // record the number of failures
+    int failures = CU_get_number_of_failures();
+
+    // clean the registry
+    CU_cleanup_registry();
+    return failures == 0 ? 0 : 1;
+}
